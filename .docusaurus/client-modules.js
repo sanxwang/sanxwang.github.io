4ee65918f69d2e486439542c5d2c185ba9d9bf7a
sanxwang <sanx1992@outlook.com>
2022-03-15 16:31:18 +0800

    a

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/sanxwang/wangannan/my-site/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/sanxwang/wangannan/my-site/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/sanxwang/wangannan/my-site/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/sanxwang/wangannan/my-site/src/css/custom.css"),
-];
